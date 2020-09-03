3e91382943cc6d5f008cb595f7c0401f1f77a3c0
akinbowale <akinbowale.akt@gmail.com>
2020-09-03 02:27:33 +0100

    chore: styling

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const copyEmailButton = document.getElementById("copy-email");
 
 copyEmailButton.addEventListener("click", () => {
   let text = document.getElementById("email-address");
-  alert(text.innerHTML);
+  text.select();
+  text.setSelectionRange(0, 99999);
+  document.execCommand("copy");
 });
